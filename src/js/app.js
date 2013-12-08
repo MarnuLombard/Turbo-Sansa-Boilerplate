475495d60e3f465927733aa86f9bdd2e6dd16ad3
MarnuLombard <marnulombard@gmail.com>
2013-12-08 20:14:46 +0200

    remove stich css I never used it, if i feel the need for something like that I'll be using SCUT {http://davidtheclark.github.io/scut/}

--- src/js/app.js
+++ src/js/app.js
@@ -1,23 +1,25 @@
-// @Description: Swap out svg img src.
-//
-// As seen here: https://gist.github.com/anonymous/3388e44bec5e9d1f7a33
-// So no author credit can be found.
-// I like it because it doesn't use jquery and doesn't need Modernizr
-// I am as of yet to see the possibnility of producing the 'error' event
-// for reasons beyond not understanding svg
-var imgs = document.querySelectorAll( "img" );
-img.addEventListener("error", function( e ) {
-	var src = this.getAttribute( "src" ),
-	fallbksrc = this.getAttribute( "data-fallbk" );
-
-	if( src == fallbacksrc ) {
-		return;
-	}
-	this.setAttribute( "src", fallbksrc );
-});
-//--> svg swap
-
-
 $(document).ready(function() {
 
-}); //docReady
+	// Replace png with svg using modernizr
+	/*if (!Modernizr.inlinesvg) {
+		$('img[src*="svg"]').attr('src', function() {
+			return $(this).attr('src').replace('.svg', '.png');
+		});
+	}*/
+	//--> Modernizr
+
+	// a link to replace a link with
+	// a phone number using js.
+	// for extra security
+	$('#showNumber').click(function() {
+		$(this).html('076 561 8470');
+		$(this).css('cursor', 'text');
+		$(this).css('text-decoration', 'none');
+	});
+
+	// a class / hook to close the modal when the open call comes from within a first modal
+
+	$('.closeAll').bind('click', function () {
+		$(this).parent().hide('fast');
+	});
+});//doc ready
